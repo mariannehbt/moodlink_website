1c74abcec9c457bf71614c2d971b6bbfc86506c6
mariannehbt <marianne.heribert@gmail.com>
2020-09-23 16:20:59 +0200

    add mailchimp

--- contribute_participate.php
+++ contribute_participate.php
@@ -24,41 +24,65 @@
                 </p>
                 <div class="row agileinfo_mail_grids">
                     <div class="col-lg-12 agileinfo_mail_grid_right">
-                        <form action="#" method="post">
-                            <div class="row">
-                                <div class="col-md-6 wthree_contact_left_grid pr-md-0">
-                                    <div class="form-group">
-                                        <input type="text" name="name" class="form-control" placeholder="Nom" required="">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                        <!-- Begin Mailchimp Signup Form -->
+                        <div id="mc_embed_signup">
+                            <form action="https://gmail.us2.list-manage.com/subscribe/post?u=204b911fb2a3858d052a8651d&amp;id=f80cf58748" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
+                                    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                                    <div class="row">
+                                        <div class="col-md-6 wthree_contact_left_grid pr-md-0">
+                                            <div class="form-group">
+                                                <label for="mce-FNAME">Prénom  <span class="asterisk">*</span></label>
+                                                <input type="text" value="" name="FNAME" class="form-control required" placeholder="Prénom" id="mce-FNAME">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="mce-EMAIL">Email  <span class="asterisk">*</span></label>
+                                                <input type="email" value="" name="EMAIL" class="form-control required email" placeholder="Email" id="mce-EMAIL">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 wthree_contact_left_grid">
+                                            <div class="form-group">
+                                                <label for="mce-LNAME">Nom  <span class="asterisk">*</span></label>
+                                                <input type="text" value="" name="LNAME" class="form-control required" placeholder="Nom" id="mce-LNAME">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="mce-PHONE">Téléphone  <span class="asterisk">*</span></label>
+                                                <input type="text" value="" name="PHONE" class="form-control required" placeholder="Téléphone" id="mce-PHONE">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="mce-SUBJECT">Sujet  <span class="asterisk">*</span></label>
+                                                <input type="text" value="" name="SUBJECT" class="form-control required" placeholder="Sujet" id="mce-SUBJECT">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="mce-MESSAGE">Message  <span class="asterisk">*</span></label>
+                                                <textarea value="" name="MESSAGE" placeholder="Message" class="form-control required" id="mce-MESSAGE"></textarea>
+                                            </div>
+                                        </div>
+                                        <div id="mce-responses" class="clear">
+                                            <div class="response" id="mce-error-response" style="display:none"></div>
+                                            <div class="response" id="mce-success-response" style="display:none"></div>
+                                        </div>
+                                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_204b911fb2a3858d052a8651d_f80cf58748" tabindex="-1" value=""></div>
+                                        <div class="col-md-12">
+                                            <div class="submit-buttons">
+                                                <button type="submit" class="btn" id="mc-embedded-subscribe">Envoyer</button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6 wthree_contact_left_grid">
-                                    <div class="form-group">
-                                        <input type="text" name="phone number" class="form-control" placeholder="Téléphone" required="">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" name="subject" class="form-control" placeholder="Sujet" required="">
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <textarea name="message" placeholder="Message" class="form-control" required=""></textarea>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="submit-buttons">
-                                        <button type="submit" class="btn">Envoyer</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
+                        <!--End mc_embed_signup-->
                     </div>
                 </div>
             </div>
         </section>
-        <!-- //contact -->
+        <!-- // Contact Form -->
         <!-- // Page Content -->
         <?php include("component_footer.php"); ?>
         <?php include("component_scroll_to_top_button.php"); ?>
